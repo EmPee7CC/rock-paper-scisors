864be6d14b965001aa4ce37a8d657bc4ea7c2aa7
EmPee7CC <michal_p11@protonmail.com>
2022-12-08 18:57:18 +0100

    Add UI

--- script.js
+++ script.js
@@ -1,42 +1,105 @@
+let pScore = 0;
+let cScore = 0;
+
 function getComputerChoice() {
   let tab = ['rock', 'paper', 'scisors'];
   return tab[Math.floor(Math.random() * tab.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection)
-    return `It's a tiebreak. You both picked ${playerSelection}`;
-  else if (playerSelection == 'rock' && computerSelection == 'scisors')
-    return `You won. ${playerSelection} beats ${computerSelection}`;
-  else if (playerSelection == 'paper' && computerSelection == 'rock')
-    return `You won. ${playerSelection} beats ${computerSelection}`;
-  else if (playerSelection == 'scisors' && computerSelection == 'paper')
-    return `You won. ${playerSelection} beats ${computerSelection}`;
-  else return `You lose. ${computerSelection} beats ${playerSelection}`;
+  if (playerSelection == computerSelection) {
+    return 0;
+  }
+  if (
+    (playerSelection == 'rock' && computerSelection == 'scisors') ||
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scisors' && computerSelection == 'paper')
+  ) {
+    pScore++;
+    return 1;
+  } else {
+    cScore++;
+    return -1;
+  }
 }
 
-function game() {
-  count = 1;
-  computerWons = 0;
-  playerWons = 0;
+// function game() {
+//   count = 1;
+//   computerWons = 0;
+//   playerWons = 0;
 
-  do {
-    userChoice = prompt('Rock, paper or scisors?').toLowerCase();
-    round = playRound(userChoice, getComputerChoice());
-    console.log(round);
-    if (round.slice(0, 8) === 'You won.') {
-      playerWons++;
-    } else if (round.slice(0, 16) === "It's a tiebreak.") {
-      continue;
+//   do {
+//     userChoice = prompt('Rock, paper or scisors?').toLowerCase();
+//     round = playRound(userChoice, getComputerChoice());
+//     console.log(round);
+//     if (round.slice(0, 8) === 'You won.') {
+//       playerWons++;
+//     } else if (round.slice(0, 16) === "It's a tiebreak.") {
+//       continue;
+//     } else {
+//       computerWons++;
+//     }
+//     count++;
+//   } while (count <= 5);
+
+//   console.log(`Results: Player: ${playerWons} vs Computer: ${computerWons}`);
+// }
+
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scisorsBtn = document.querySelector('#scisors');
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
+const scorePara = document.querySelector('.score-para');
+
+rockBtn.addEventListener('click', () => playerChoice('rock'));
+paperBtn.addEventListener('click', () => playerChoice('paper'));
+scisorsBtn.addEventListener('click', () => playerChoice('scisors'));
+
+function playerChoice(playerSelection) {
+  const computerSelection = getComputerChoice();
+
+  game = playRound(playerSelection, computerSelection);
+  changeResult(game, playerSelection, computerSelection);
+
+  if (pScore === 5 || cScore === 5) {
+    if (pScore === 5) {
+      scorePara.textContent = 'You won this game.';
     } else {
-      computerWons++;
+      scorePara.textContent = 'Computer won this game.';
     }
-    count++;
-  } while (count <= 5);
-
-  console.log(`Results: Player: ${playerWons} vs Computer: ${computerWons}`);
+    pScore = 0;
+    cScore = 0;
+    playerScore.textContent = `Player: ${pScore}`;
+    computerScore.textContent = `Computer: ${cScore}`;
+    setTimeout(() => {
+      scorePara.textContent = '';
+    }, 3000);
+  }
 }
 
-game();
-// console.log(userChoice);
-// playRound(userChoice, getComputerChoice());
+function changeResult(result, playerSelection, computerSelection) {
+  switch (result) {
+    case 0:
+      scorePara.textContent = "It's a tiebreak.";
+      break;
+    case 1:
+      scorePara.textContent = `You won. ${capitalizeFirstLetter(
+        playerSelection
+      )} beats ${capitalizeFirstLetter(computerSelection)}`;
+      playerScore.textContent = `Player: ${pScore}`;
+      break;
+    case -1:
+      scorePara.textContent = `You lost. ${capitalizeFirstLetter(
+        computerSelection
+      )} beats ${capitalizeFirstLetter(playerSelection)}`;
+      computerScore.textContent = `Computer: ${cScore}`;
+      break;
+    default:
+      break;
+  }
+}
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
